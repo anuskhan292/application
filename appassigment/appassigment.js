e5970c3a66a208d1d67680d04592ad08a5ef6041
Anus Khan <81530277+anuskhan292@users.noreply.github.com>
2021-09-11 10:16:35 -0700

    Add files via upload

---
+++ appassigment/appassigment.js
@@ -0,0 +1,58 @@
+// checkbox
+// var a = document.getElementById('L_pass');
+
+// function myfunc(){
+//     if(a.type === 'password'){
+//         a.type = 'text';
+
+//     }else{
+//         a.type = 'password';
+//     }
+// }
+// checkbox
+
+
+// signup
+// function signup(){ 
+
+//     // console.log("runing");
+//     var name =  document.getElementById("name").value;
+//     var fName = document.getElementById("fName").value;
+//     var email = document.getElementById("email").value;
+//     var pass = document.getElementById("pass").value;
+    
+//     localStorage.setItem("name1",name);
+//     localStorage.setItem("fName1",fName);
+//     localStorage.setItem("email1",email);
+//     localStorage.setItem("pass1",pass);
+
+//     document.getElementById("name").value="";
+//     document.getElementById("fName").value="";
+//     document.getElementById("email").value="";
+//     document.getElementById("pass").value="";
+
+//     alert("User registerd sucecessfully...!!!")
+// }   
+// signup
+
+// login
+
+// function login(){
+//     var L_email = document.getElementById("L_email").value;
+//     var L_pass = document.getElementById("L_pass").value;
+
+//     var email1 = localStorage.getItem("email1");
+//     var pass1 = localStorage.getItem("pass1");
+
+//     if(L_email==email1 && L_pass==pass1){
+
+//         localStorage.setItem("code","secret");
+//         window.location.href="home.html";
+//     }else{
+//         alert("email and pass is incorrect");
+//     }
+
+// }
+
+
+// logout
